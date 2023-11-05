81ec6f81e93054a0a5fd314f4082a10ff9bc1bd7
Hokkinss <146117952+Hokkinss@users.noreply.github.com>
2023-11-05 21:06:13 +0500

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,32 @@
+import { COMMENT_LINES, NAMES, AVATAR_COUNT, COMMENT_COUNT } from './constants.js';
+import { getRandomInteger, getRandomArrayElement, createIdGenerator } from './utils.js';
+
+const generateCommentId = createIdGenerator();
+
+export const createMessage = () => Array.from(
+  { length: getRandomInteger(1, 2) },
+  () => getRandomArrayElement(COMMENT_LINES),
+).join(' ');
+
+export const createComment = () => ({
+  id: generateCommentId(),
+  avatar: `img/avatar-${getRandomInteger(1, AVATAR_COUNT)}.svg`,
+  message: createMessage(),
+  name: getRandomArrayElement(NAMES),
+});
+
+export const createPicture = (index) => ({
+  id: index,
+  url: `photos/${index}.jpg`,
+  description: getRandomArrayElement(DESCRIPTIONS),
+  likes: getRandomInteger(LIKE_MIN_COUNT, LIKE_MAX_COUNT),
+  comments: Array.from(
+    { length: getRandomInteger(0, COMMENT_COUNT) },
+    createComment,
+  ),
+});
+
+export const getPictures = () => Array.from(
+  { length: PICTURE_COUNT },
+  (_, pictureIndex) => createPicture(pictureIndex + 1),
+);
