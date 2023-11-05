b7366396495cfbf18fff41baa5b187747f291e01
Hokkinss <146117952+Hokkinss@users.noreply.github.com>
2023-11-05 22:23:20 +0500

    Update function.js

--- function.js
+++ function.js
@@ -26,3 +26,18 @@ function palindrom(word) {
 }
 
 console.log(palindrom("ванна"));
+
+function isMeetingOk(startWork, endWork, startMeeting, duration) {
+  startWork = startWork.split(':');
+  endWork = endWork.split(':');
+  startMeeting = startMeeting.split(':');
+
+  const startWorkMinutes = parseInt(startWork[0]) * 60 + parseInt(startWork[1]);
+  const endWorkMinutes = parseInt(endWork[0]) * 60 + parseInt(endWork[1]);
+  const startMeetingMinutes = parseInt(startMeeting[0]) * 60 + parseInt(startMeeting[1]);
+  const endMeetingMinutes = startMeetingMinutes + duration;
+
+  return startWorkMinutes <= startMeetingMinutes && startMeetingMinutes < endWorkMinutes && startWorkMinutes < endMeetingMinutes && endMeetingMinutes <= endWorkMinutes;
+}
+
+console.log(isMeetingOk('9:00', '18:00', '10:30', 90));
