7b1c583da7c70fdea88f814857680e8c3e63d261
Hokkinss <146117952+Hokkinss@users.noreply.github.com>
2023-11-24 17:53:13 +0500

    module7
    11

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 import { getPictures } from './data.js';
+import {renderThumbnails} from './thumbnails.js';
+
+renderThumbnails(getPictures());
 const pictures = getPictures();
 
 console.log(pictures); // Вывод данных в консоль для проверки
