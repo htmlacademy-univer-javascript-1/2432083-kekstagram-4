13706814884c6bdc2e163ebbe07e9d915fc637ca
Hokkinss <146117952+Hokkinss@users.noreply.github.com>
2023-12-27 16:32:10 +0500

    Add files via upload

---
+++ utils/misc.js
@@ -0,0 +1,75 @@
+const ALERT_SHOW_TIME = 5000;
+export const createIdGenerator = () => {
+  let lastId = 0;
+  return function () {
+    lastId += 1;
+    return lastId;
+  };
+};
+export const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+export const createUniqueRandomIdGenerator = (min, max) => {
+  const generatedValues = [];
+  return function () {
+    let currentValue = getRandomInteger(min, max);
+    if (generatedValues.length >= (max - min + 1)) {
+      throw new Error(`Перебраны все числа из диапазона от ${min} до ${max}`);
+    }
+    while (generatedValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    generatedValues.push(currentValue);
+    return currentValue;
+  };
+};
+export const getRandomElement = (array) => array[getRandomInteger(0, array.length - 1)];
+export const isEscapeKey = (evt) => evt.key === 'Escape';
+export const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = '100';
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = '0';
+  alertContainer.style.top = '0';
+  alertContainer.style.right = '0';
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '30px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+  alertContainer.textContent = message;
+  document.body.append(alertContainer);
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_TIME);
+};
+export const onEscKeyDown = (evt, cb) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    cb();
+  }
+};
+
+export function debounce (callback, timeoutDelay = 500) {
+  let timeoutId;
+
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+export function throttle (callback, delayBetweenFrames) {
+  let lastTime = 0;
+
+  return (...rest) => {
+    const now = new Date();
+
+    if (now - lastTime >= delayBetweenFrames) {
+      callback.apply(this, rest);
+      lastTime = now;
+    }
+  };
+}
